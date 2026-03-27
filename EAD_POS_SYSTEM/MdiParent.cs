1e10d49d5e044254dd86e8b502934a688d935e99
GabrielleL2212 <gmnss1415@gmail.com>
2026-03-27 12:09:58 -0500

    .

--- EAD_POS_SYSTEM/MdiParent.cs
+++ EAD_POS_SYSTEM/MdiParent.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq; // You need this using directive for OfType<T>() and SingleOrDefault()
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
